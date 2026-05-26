7db70d0d27599a82b5bc5277f2b9c518ee0658c4
ivaneduc360 <ivan.educ360@gmail.com>
2026-05-26 17:17:46 -0300

    Update dash.py

--- dash.py
+++ dash.py
@@ -2,6 +2,7 @@ import pandas as pd
 import plotly.express as px
 import sqlalchemy
 import streamlit as st
+import urllib.parse
 
 # 1. Conexão com o Banco de Dados
 # (Substitua com suas credenciais locais ou de produção)
